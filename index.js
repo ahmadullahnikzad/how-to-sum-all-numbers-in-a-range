821b1ef8e17422ea07bed5fe4089960c190073da
ahmadullah <ahmadullahnikzad1919@gmail.com>
2020-09-20 14:45:06 +0000

    solution 2 added

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//solution 1
 function sumAll(arr) {
   let total=0;
   if(arr[0]<arr[arr.length-1]){
@@ -14,3 +15,12 @@ function sumAll(arr) {
 }
 
 console.log(sumAll([40,1]));
+//solution 2
+function sumAll2(arr){
+  let total=0;
+  for(let i=Math.min.apply(null,arr);i<=Math.max.apply(null,arr);i++){
+total+=i;
+  }
+  return total;
+}
+sumAll2([4,1])
